46d7c50e8b995a5ac91d234b95ec0140ec017663
grkemayk <93814922+grkemayk@users.noreply.github.com>
2022-04-27 21:28:26 +0300

    Swagger

--- src/main/java/com/example/SpringDataPostgre/controller/restapi.java
+++ src/main/java/com/example/SpringDataPostgre/controller/restapi.java
@@ -2,7 +2,8 @@ package com.example.SpringDataPostgre.controller;
 
 import com.example.SpringDataPostgre.dto.kisiDto;
 import com.example.SpringDataPostgre.service.kisiService;
-import org.springframework.beans.factory.annotation.Autowired;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,18 +11,21 @@ import javax.annotation.Resource;
 
 @RestController
 @RequestMapping("/myApi")
-public class api {
+@Api(value = "kisiDto Api documentation")
+public class restapi {
 
     @Resource(name = "kisiService")
     kisiService KisiService;
 
     @PostMapping
+    @ApiOperation(value = "New kisiDto adding method")
     private ResponseEntity<kisiDto> save(@RequestBody kisiDto KisiDto)
     {
         return ResponseEntity.ok(KisiService.save(KisiDto));
     }
 
     @GetMapping("/{id}")
+    @ApiOperation(value = "Get kisiDto method")
     private ResponseEntity<kisiDto> get(@PathVariable(value = "id") Long id)
     {
         return ResponseEntity.ok(KisiService.get(id));
--- src/main/java/com/example/SpringDataPostgre/dto/kisiDto.java
+++ src/main/java/com/example/SpringDataPostgre/dto/kisiDto.java
@@ -1,14 +1,21 @@
 package com.example.SpringDataPostgre.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.util.List;
 
 @Data
+@ApiModel(value = "User Api model documentation", description = "Model")
 public class kisiDto {
+    @ApiModelProperty(value = "Unique id field of kisiDto object")
     private Long id;
+    @ApiModelProperty(value = "Name field of kisiDto object")
     private String adi;
+    @ApiModelProperty(value = "Surname field of kisiDto object")
     private String soyadi;
+    @ApiModelProperty(value = "Adresses field of kisiDto object")
     private List<String> adresleri;
 }
 
